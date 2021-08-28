b83bfed326af4e07342da3cb849604c2b92ab669
sent1nu11 <sent1nu11@gmail.com>
2021-08-28 13:35:46 -0600

    final version

--- player.py
+++ player.py
@@ -9,4 +9,21 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self):
         super().__init__()
-        
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
